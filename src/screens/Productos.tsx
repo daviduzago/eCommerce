7bd33b7ce182a77724f1372593ccd48a11608a6d
David Zago <David.Zago@vailresorts.com>
2024-06-02 21:52:59 -0500

    Agregamos tarjeta de producto

--- src/screens/Productos.tsx
+++ src/screens/Productos.tsx
@@ -1,7 +1,20 @@
+import ProductCard from "../Components/ProductCard"
+
 const Productos = () => {
+    // Llamar el api
     return (
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="max-w-screen-xl items-center justify-center mx-auto p-4">
             <h1>Productos</h1>
+            <div className="flex flex-wrap justify-center">
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+                <ProductCard id={2} name="Test" rating={4} price={200} />
+            </div>
         </div>
     )
 }
