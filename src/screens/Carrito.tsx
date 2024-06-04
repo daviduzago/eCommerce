bb9440d3aa9aebe6c7230c014d5361c47ad7a2c1
David Zago <David.Zago@vailresorts.com>
2024-06-03 22:35:19 -0500

    Agregamos carrito con redux

--- src/screens/Carrito.tsx
+++ src/screens/Carrito.tsx
@@ -1,7 +1,18 @@
+import ProductCard from "../Components/ProductCard"
+import { useAppSelector } from "../hooks/redux"
+
 const Carrito = () => {
+    const { carrito } = useAppSelector((state) => state.carrito)
     return (
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <h1>Carrito</h1>
+        <div className="max-w-screen-xl items-center justify-center mx-auto p-4">
+            <div className="flex flex-1 items-center justify-between mx-auto p-4">
+                <h1 className="flex flex-1 items-center justify-center text-3xl font-bold">Carrito</h1>
+            </div>
+            <div className="flex flex-wrap justify-center">
+                {carrito.map((product, index) => (
+                    <ProductCard key={"product" + index} id={product.id} name={product.name} rating={product.rating} price={product.price} />
+                ))}
+            </div>
         </div>
     )
 }
